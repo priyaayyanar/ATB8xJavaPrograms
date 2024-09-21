2244fef8c203a89a42ba4fe5329b9fa447cc7662
Priya Ayyanar <priyamcanandha@gmail.com>
2024-09-21 20:46:20 +0530

    Add the files

---
+++ src/sept/ex_06092024/Lab007.java
@@ -0,0 +1,11 @@
+package sept.ex_06092024;
+/* Author - Priya Ayyanar
+Program - ATB8x
+Usage : Explain the comments in Java
+ */
+public class Lab007 {
+
+    public static void main(String[] priya) {
+        System.out.println("Hi Priya!");
+    }
+}
---
+++ src/sept/ex_06092024/Lab008.java
@@ -0,0 +1,14 @@
+//Below code is package : folder with sept -> exercise for 06th of Sept month 2024 (ex_06092024) subfolder
+package sept.ex_06092024;
+/* Author - Priya Ayyanar
+Program - ATB8x
+Usage : Explain the comments in Java
+ */
+//Program with the class file, name of the class is Lab008
+public class Lab008 {
+    //This is the main function, JVM starts from here(entry point for JVM.
+    public static void main(String[] args) {
+        //Below code will be executed and print the output as Hello World!
+        System.out.println();
+    }
+}
